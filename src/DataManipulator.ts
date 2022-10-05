d06009d21783b1957324c3b52f321c5463252473
bruhADexe <89359804+bruhADexe@users.noreply.github.com>
2022-10-06 00:55:32 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -9,11 +9,20 @@ export interface Row {
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
+    const price_abc = (serverResponse[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const price_def = (serverResponse[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ration = price_abc / price_def;
+    const upper_bound = 1 + 0.05
+    const lower_bound = 1 - 0.05
     return serverResponds.map((el: any) => {
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc : number,
+        price_def : number,
+        raatio : number,
+        timestamp: Date,
+        upper_bound : number,
+        lower_bound : number,
+        trigger_alert : number | undefined,
       };
     })
   }
